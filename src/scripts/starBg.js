e1eb7f068f024a08ee781c5336860e7e1e559e5b
Cedcn <cedcn@qq.com>
2017-08-15 15:52:46 +0800

    Adjust style

--- src/scripts/starBg.js
+++ src/scripts/starBg.js
@@ -5,16 +5,17 @@ const starBg = () => {
   const $starsBox = $('#js-starsBox');
 
   for (let i = 0; i <= stars; i++) {
-    const size = Math.random() * 3;
+    const size = Math.random() * 4;
 
     $starsBox.prepend(`<span style="width:${size}px; height: ${size}px; top: ${Math.random() * 100}%; left: ${Math.random() * 100}%;"></span>`);
   };
 
-  setTimeout(() => {
+  const delay = setTimeout(() => {
     $starsBox.find('span').each(function() {
       $(this).css('top', Math.random() * 100 + '%').css('left', Math.random() * 100 + '%');
     });
-  }, 1);
+    clearTimeout(delay);
+  }, 100);
 
   setInterval(() => {
     $starsBox.find('span').each(function() {
